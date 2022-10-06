bca2ded2581b83d48a6aa893d8916de0d69257f6
KBMackenzie <109933089+KBMackenzie@users.noreply.github.com>
2022-10-06 00:36:08 -0300

    Clean up

--- SaveData/HeartState.cs
+++ SaveData/HeartState.cs
@@ -6,9 +6,14 @@ namespace RedHeartsFirst
 {
     public enum HeartOrder
     {
-        Off,
+        Off, // Same as BlackBlueRed, which is the default
         BlackRedBlue,
         RedBlackBlue,
         BlueRedBlack,
+
+        /* I'm not including: 
+         * RedBlueBlack,
+         * BlueBlackRed,
+         * ... Because why would anyone want those?? */
     }
 }
